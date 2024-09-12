32c05867a6b76581ec4246c314b23927d72ff7a4
Roan Poggere da Silva <371153@unoesc.lan>
2024-09-11 22:07:42 -0300

    final 1109

---
+++ Atv4/Pilhas/AtvPilha/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Informe uma palavra: ");
+Stack<string> palavra = Console.ReadLine();
+
+string invertido = string.Empty;
+while(palavra.Count > 0)
+
+foreach (char c in palavra){
+    invertido += caracteres.Peek();
+}
+
+Console.WriteLine(palavra);
+Console.WriteLine(invertido);
+
---
+++ Atv4/Pilhas/Program.cs
@@ -0,0 +1,22 @@
+﻿// Basicamente as pilhas trabalham com uma 
+// estrutura LIFO -> Las In First Out
+// Três ,étodos importantes
+// Push() -> Insere um elemento no topo da pilha
+// Pop() -> Remove um elemento do topo e o retorna
+// Peek() -> Retorna o elemento do topo sem removelo
+
+// Invertendo as bolas
+Stack<char> caracteres = new Stack<char>();
+foreach(char c in "DARCY DA MONTANHA")
+{
+    caracteres.Push(c);
+}
+
+//desempilhando
+string invertido = string.Empty;
+while(caracteres.Count > 0)
+{
+    invertido += caracteres.Pop();
+}
+
+Console.WriteLine(invertido);
