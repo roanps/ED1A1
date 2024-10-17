7e212a2b34d5aab8aebd4810216df953d5ffe376
Roan Poggere da Silva <371153@unoesc.lan>
2024-10-16 22:15:01 -0300

    quick

---
+++ Atv4/AlgoritimosOrdenacao/QuickSort/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using QuickSort;
+
+
+Console.WriteLine("Hello, World!");
+int[] values = new int[] {0, 15, 24, 1, 3, 6, 12, 9};
+
+QuickSort.QuickSort.Print<int>(values);
+Console.WriteLine("Inicializando ordenação.");
+QuickSort.QuickSort.Sort<int>(values, 0, (values.Length -1) );
+Console.WriteLine("Finalizando ordenção. ");
+QuickSort.QuickSort.Print<int>(values);
+
+
